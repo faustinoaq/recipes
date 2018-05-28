38ab1d2069590d7001aad4b2735105f3dd1cf8f8
Faustino Aguilar <faustinoaq@gmail.com>
2018-05-28 07:14:46 -0500

    Remove migrate
    - Remove micrate because Jennifer uses its own migration system .
    - Waiting recipes support for auth and migrations

--- jennifer/app/spec/spec_helper.cr
+++ jennifer/app/spec/spec_helper.cr
@@ -1,15 +1,9 @@
 ENV["AMBER_ENV"] ||= "test"
 
 require "spec"
-# require "micrate"
 require "garnet_spec"
 
 require "../config/*"
-
-# Micrate::DB.connection_url = Amber.settings.database_url
-
-# Automatically run migrations on the test database
-# Micrate::Cli.run_up
 
 module Spec
   DRIVER = :chrome
